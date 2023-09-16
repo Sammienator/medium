c89872fb42a08c080f16d36b0c844a8f08d3ee43
Sammienator <sammugo18@gmail.com>
2023-09-16 07:15:12 +0300

    added files

---
+++ src/components/Portfolio.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+  render() {
+    const { resumeData } = this.props;
+
+    return (
+      <section id="portfolio">
+        <div className="row">
+          <div className="twelve columns collapsed">
+            <h1>Check Out Some of My Works.</h1>
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf" alt="">
+              {resumeData.portfolio &&
+                resumeData.portfolio.map((item) => (
+                  <div className="columns portfolio-item" key={item.name}>
+                    <div className="item-wrap">
+                      <a href="#modal-01">
+                        <img src={`${item.imgurl}`} className="item-img" alt="" />
+                        <div className="overlay">
+                          <div className="portfolio-item-meta">
+                            <h5>{item.name}</h5>
+                            <p>{item.description}</p>
+                          </div>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
